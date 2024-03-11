f3b623eb041237e7426e445ed6e727a6df22a5a1
kenil-kanani <kenilkananigecbvn@gmail.com>
2024-03-12 00:53:05 +0530

    Added absolute import configuration using vite config and jsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import './App.css';
+
+import Home from 'Pages/Home';
 
 function App() {
 
   return (
     <>
-
+      Kenil
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
